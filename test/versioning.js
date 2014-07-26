d2bf45983b5a504b5a6d19c38d9e13fceb16b042
Mathias Buus <mathiasbuus@gmail.com>
2014-07-27 00:53:25 +0200

    version tests

---
+++ test/versioning.js
@@ -0,0 +1,60 @@
+var test = require('./helpers')
+var concat = require('concat-stream')
+
+test('two versions', function(t, db) {
+  db.put('foo', 'bar', function(err) {
+    t.notOk(err, 'no err')
+    db.put('foo', 'baz', {version:1}, function(err) {
+      t.notOk(err, 'no err')
+      db.get('foo', function(err, val, version) {
+        t.notOk(err, 'no err')
+        t.same(val, 'baz')
+        t.same(version, 2)
+        t.end()
+      })
+    })
+  })
+})
+
+test('conflict', function(t, db) {
+  db.put('foo', 'bar', function(err) {
+    t.notOk(err, 'no err')
+    db.put('foo', 'baz', function(err) {
+      t.ok(err, 'conflict err')
+      t.ok(err.conflict, 'conflict flag set')
+      t.same(err.key, 'foo', 'key set')
+      t.end()
+    })
+  })
+})
+
+test('no conflict on force', function(t, db) {
+  db.put('foo', 'bar', function(err) {
+    t.notOk(err, 'no err')
+    db.put('foo', 'baz', {force:true}, function(err) {
+      t.notOk(err, 'no err')
+      db.get('foo', function(err, val, version) {
+        t.notOk(err, 'no err')
+        t.same(val, 'baz')
+        t.same(version, 2)
+        t.end()
+      })
+    })
+  })
+})
+
+test('version stream', function(t, db) {
+  db.put('foo', 'bar', function(err) {
+    t.notOk(err, 'no err')
+    db.put('foo', 'baz', {version:1}, function(err) {
+      t.notOk(err, 'no err')
+      db.createVersionStream('foo').pipe(concat(function(versions) {
+        t.same(versions.length, 2, '2 versions')
+        t.same(versions[0], {key:'foo', value:'bar', version:1})
+        t.same(versions[1], {key:'foo', value:'baz', version:2})
+        t.end()
+      }))
+    })
+  })
+})
+
