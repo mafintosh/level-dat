42aece5efc218209b787511b44eeb83d547fed4f
Mathias Buus <mathiasbuus@gmail.com>
2014-07-27 00:59:09 +0200

    misc tests

---
+++ test/misc.js
@@ -0,0 +1,46 @@
+var test = require('./helpers')
+
+test('count', function(t, db) {
+  db.put('hi', 'world', function() {
+    db.count(function(err, cnt) {
+      t.notOk(err, 'no err')
+      t.same(cnt, 1, 'count is 1')
+      db.put('hello', 'world', function() {
+        db.count(function(err, cnt) {
+          t.notOk(err, 'no err')
+          t.same(cnt, 2, 'count is 2')
+          t.end()
+        })
+      })
+    })
+  })
+})
+
+test('count + del', function(t, db) {
+  db.put('hi', 'world', function() {
+    db.put('hello', 'world', function() {
+      db.count(function(err, cnt) {
+        t.notOk(err, 'no err')
+        t.same(cnt, 2, 'count is 2')
+        db.del('hello', function() {
+          db.count(function(err, cnt) {
+            t.notOk(err, 'no err')
+            t.same(cnt, 1, 'count is 1')
+            t.end()
+          })
+        })
+      })
+    })
+  })
+})
+
+test('meta', function(t, db) {
+  db.putMeta('hello', 'world', function(err) {
+    t.notOk(err, 'no err')
+    db.getMeta('hello', function(err, val) {
+      t.notOk(err, 'no err')
+      t.same(val, 'world')
+      t.end()
+    })
+  })
+})
